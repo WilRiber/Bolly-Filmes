39a4c0d288e6fa36cd99ae4bb9ab6adf22fea05f
Willian Ribeiro <ribeiro3001@hotmail.com>
2017-07-09 16:26:55 -0300

    Adicionando arquivos no projeto

---
+++ app/src/main/java/ribeiro/willian/com/br/bollyfilmes/FilmeDetalheFragment.java
@@ -0,0 +1,127 @@
+package ribeiro.willian.com.br.bollyfilmes;
+
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import ribeiro.willian.com.br.bollyfilmes.data.FilmesContract;
+
+public class FilmeDetalheFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor> {
+
+    private Uri filmeUri;
+
+    private TextView tituloView;
+
+    private TextView dataLancamentoView;
+
+    private TextView descricaoView;
+
+    private RatingBar avaliacaoView;
+
+    private ImageView capaView;
+
+    private ImageView posterView;
+
+    private static final int FILME_DETALHE_LOADER = 0;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (getArguments() != null) {
+            filmeUri = (Uri) getArguments().getParcelable(MainActivity.FILME_DETALHE_URI);
+        }
+
+        getLoaderManager().initLoader(FILME_DETALHE_LOADER, null, this);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_filme_detalhe, container, false);
+
+        tituloView = (TextView) view.findViewById(R.id.item_titulo);
+
+        dataLancamentoView = (TextView) view.findViewById(R.id.item_data);
+
+        descricaoView = (TextView) view.findViewById(R.id.item_desc);
+
+        avaliacaoView = (RatingBar) view.findViewById(R.id.item_avaliacao);
+
+        capaView = (ImageView) view.findViewById(R.id.item_capa);
+
+        posterView = (ImageView) view.findViewById(R.id.item_poster);
+
+        return view;
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+
+        if(filmeUri == null) return null;
+
+        String[] projection = {
+                FilmesContract.FilmeEntry._ID,
+                FilmesContract.FilmeEntry.COLUMN_TITULO,
+                FilmesContract.FilmeEntry.COLUMN_DESCRICAO,
+                FilmesContract.FilmeEntry.COLUMN_POSTER_PATH,
+                FilmesContract.FilmeEntry.COLUMN_CAPA_PATH,
+                FilmesContract.FilmeEntry.COLUMN_AVALIACAO,
+                FilmesContract.FilmeEntry.COLUMN_DATA_LANCAMENTO
+        };
+
+        return new CursorLoader(getContext(), filmeUri, projection, null, null, null);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        if (data == null || data.getCount() < 1) {
+            return;
+        }
+
+        if (data.moveToFirst()) {
+
+            int tituloIndex = data.getColumnIndex(FilmesContract.FilmeEntry.COLUMN_TITULO);
+            int descricaoIndex = data.getColumnIndex(FilmesContract.FilmeEntry.COLUMN_DESCRICAO);
+            int posterIndex = data.getColumnIndex(FilmesContract.FilmeEntry.COLUMN_POSTER_PATH);
+            int capaIndex = data.getColumnIndex(FilmesContract.FilmeEntry.COLUMN_CAPA_PATH);
+            int avaliacaoIndex = data.getColumnIndex(FilmesContract.FilmeEntry.COLUMN_AVALIACAO);
+            int dataLancamentoIndex = data.getColumnIndex(FilmesContract.FilmeEntry.COLUMN_DATA_LANCAMENTO);
+
+            String titulo = data.getString(tituloIndex);
+            String descricao = data.getString(descricaoIndex);
+            String poster = data.getString(posterIndex);
+            String capa = data.getString(capaIndex);
+            float avaliacao = data.getFloat(avaliacaoIndex);
+            String dataLancamento = data.getString(dataLancamentoIndex);
+
+            tituloView.setText(titulo);
+            descricaoView.setText(descricao);
+            avaliacaoView.setRating(avaliacao);
+            dataLancamentoView.setText(dataLancamento);
+
+            new DownloadImageTask(capaView).execute(capa);
+
+            if (posterView != null) {
+                new DownloadImageTask(posterView).execute(poster);
+            }
+
+        }
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+
+    }
+}
---
+++ app/src/main/java/ribeiro/willian/com/br/bollyfilmes/ItemFilme.java
@@ -0,0 +1,160 @@
+package ribeiro.willian.com.br.bollyfilmes;
+
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.Serializable;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+/*{
+        "poster_path": "/IfB9hy4JH1eH6HEfIgIGORXi5h.jpg",
+        "adult": false,
+        "overview": "Jack Reacher must uncover the truth behind a major government conspiracy in order to clear his name. On the run as a fugitive from the law, Reacher uncovers a potential secret from his past that could change his life forever.",
+        "release_date": "2016-10-19",
+        "genre_ids": [
+        53,
+        28,
+        80,
+        18,
+        9648
+        ],
+        "id": 343611,
+        "original_title": "Jack Reacher: Never Go Back",
+        "original_language": "en",
+        "title": "Jack Reacher: Never Go Back",
+        "backdrop_path": "/4ynQYtSEuU5hyipcGkfD6ncwtwz.jpg",
+        "popularity": 33.793474,
+        "vote_count": 304,
+        "video": false,
+        "vote_average": 4.38
+}*/
+
+public class ItemFilme implements Serializable {
+
+    private long id;
+
+    private String titulo;
+
+    private String descricao;
+
+    private String dataLancamento;
+
+    // http://image.tmdb.org/t/p/w185/IfB9hy4JH1eH6HEfIgIGORXi5h.jpg
+    private String posterPath;
+
+    // http://image.tmdb.org/t/p/w185/4ynQYtSEuU5hyipcGkfD6ncwtwz.jpg
+    private String capaPath;
+
+    private float avaliacao;
+
+    private float popularidade;
+
+    public ItemFilme(long id, String titulo, String descricao, String dataLancamento, String posterPath, String capaPath, float avaliacao, float popularidade) {
+        this.id = id;
+        this.titulo = titulo;
+        this.descricao = descricao;
+        this.dataLancamento = dataLancamento;
+        this.posterPath = posterPath;
+        this.capaPath = capaPath;
+        this.avaliacao = avaliacao;
+        this.popularidade = popularidade;
+    }
+
+    public ItemFilme(JSONObject jsonObject) throws JSONException {
+        this.id = jsonObject.getLong("id");
+        this.titulo = jsonObject.getString("title");
+        this.descricao = jsonObject.getString("overview");
+        this.dataLancamento = jsonObject.getString("release_date");
+        this.posterPath = jsonObject.getString("poster_path");
+        this.capaPath = jsonObject.getString("backdrop_path");
+        this.avaliacao = (float) jsonObject.getDouble("vote_average");
+        this.popularidade = (float) jsonObject.getDouble("popularity");
+    }
+
+    private String buildPath(String width, String path) {
+        StringBuilder builder = new StringBuilder();
+        builder.append("http://image.tmdb.org/t/p/")
+                .append(width)
+                .append(path);
+        return  builder.toString();
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getDataLancamento() {
+
+        Locale locale = new Locale("pt", "BR");
+
+        try {
+            Date date = new SimpleDateFormat("yyyy-MM-dd", locale).parse(dataLancamento);
+            return new SimpleDateFormat("dd/MM/yyyy", locale).format(date);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        return dataLancamento;
+    }
+
+    public void setDataLancamento(String dataLancamento) {
+        this.dataLancamento = dataLancamento;
+    }
+
+    public String getPosterPath() {
+        return buildPath("w500", posterPath);
+    }
+
+    public void setPosterPath(String posterPath) {
+        this.posterPath = posterPath;
+    }
+
+    public String getCapaPath() {
+        return buildPath("w780", capaPath);
+    }
+
+    public void setCapaPath(String capaPath) {
+        this.capaPath = capaPath;
+    }
+
+    public float getAvaliacao() {
+        return avaliacao;
+    }
+
+    public void setAvaliacao(float avaliacao) {
+        this.avaliacao = avaliacao;
+    }
+
+    public float getPopularidade() {
+        return popularidade;
+    }
+
+    public void setPopularidade(float popularidade) {
+        this.popularidade = popularidade;
+    }
+}
+
